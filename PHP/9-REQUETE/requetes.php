45fa20da97650ca832eaa3509a9ae79b7c2c2136
Romy KLK <romyklk1610@gmail.com>
2023-06-07 10:13:54 +0200

    php

--- PHP/9-REQUETE/requetes.php
+++ PHP/9-REQUETE/requetes.php
@@ -5,9 +5,41 @@
         font-size: 25px;
     }
 </style>
+
+
 <?php
 echo "<hr><h2>Connexion à la base de données avec PDO(Php Data Object)</h2>";
 /* 
+Créer une base de données societe : 
+CREATE TABLE IF NOT EXISTS `employes` (
+  `id_employes` int NOT NULL AUTO_INCREMENT,
+  `prenom` varchar(20) NOT NULL,
+  `nom` varchar(20) NOT NULL,
+  `sexe` enum('m','f') NOT NULL,
+  `service` varchar(30) NOT NULL,
+  `date_embauche` date NOT NULL,
+  `salaire` int NOT NULL,
+  PRIMARY KEY (`id_employes`)
+) ENGINE=InnoDB AUTO_INCREMENT=991 DEFAULT CHARSET=utf8mb4;
+INSERT INTO `employes` (`id_employes`, `prenom`, `nom`, `sexe`, `service`, `date_embauche`, `salaire`) VALUES
+(350, 'Jean-pierre', 'Laborde', 'm', 'direction', '2010-12-09', 5100),
+(388, 'Clement', 'Gallet', 'm', 'commercial', '2010-12-15', 2400),
+(415, 'Thomas', 'Winter', 'm', 'commercial', '2011-05-03', 3650),
+(417, 'Chloe', 'Dubar', 'f', 'production', '2011-09-05', 2000),
+(509, 'Fabrice', 'Grand', 'm', 'comptabilite', '2011-12-30', 3000),
+(547, 'Melanie', 'Collier', 'f', 'commercial', '2012-01-08', 3200),
+(592, 'Laura', 'Blanchet', 'f', 'direction', '2012-05-09', 4600),
+(627, 'Guillaume', 'Miller', 'm', 'commercial', '2012-07-02', 2000),
+(655, 'Celine', 'Perrin', 'f', 'commercial', '2012-09-10', 2800),
+(701, 'Mathieu', 'Vignal', 'm', 'informatique', '2013-04-03', 2600),
+(780, 'Amandine', 'Thoyer', 'f', 'communication', '2014-01-23', 2200),
+(802, 'Damien', 'Durand', 'm', 'informatique', '2014-07-05', 2350),
+(854, 'Daniel', 'Chevel', 'm', 'informatique', '2015-09-28', 3200),
+(876, 'Nathalie', 'Martin', 'f', 'juridique', '2016-01-12', 3650),
+(900, 'Benoit', 'Lagarde', 'm', 'production', '2016-06-03', 2650),
+(933, 'Emilie', 'Sennard', 'f', 'commercial', '2017-01-11', 1900),
+(990, 'Stephanie', 'Lafaye', 'f', 'assistant', '2017-03-01', 1875);
+
 Pour se connecter à une base de données, nous allons utiliser PDO (Php Data Object) qui est classe prédéfinie en PHP qui va nous permettre de se connecter à une base de données.L'avantage d'utiliser PDO est qu'elle compatible avec plusieurs SGBD (Système de Gestion de Base de Données) comme MySQL, Oracle, SQL Server, etc. On peut donc changer de SGBD sans avoir à changer de code.
 */
 $connect = new PDO('mysql:host=localhost;dbname=formation','root','',  
@@ -39,12 +71,12 @@ var_dump($bdd);
 
 // exec() est utilisée pour la formulation de requêtes ne retournant pas de jeu de résultats : INSERT, UPDATE, DELETE. exec() renvoie le nombre de lignes affectées par la requête ou FALSE si une erreur survient.
 
-$sqlReq = "INSERT INTO employes (prenom, nom, sexe, service, date_embauche, salaire) VALUES ('Jean', 'Dujardin', 'm', 'informatique', '2018-02-20', 2000)";
+/* $sqlReq = "INSERT INTO employes (prenom, nom, sexe, service, date_embauche, salaire) VALUES ('Jean', 'Dujardin', 'm', 'informatique', '2018-02-20', 2000)";
 
 $request = $bdd->exec($sqlReq);
 
 echo "Nombre d'enregistrements affectés par l'INSERT : $request <br>";
-echo "L'id du dernier enregistrement est : " . $bdd->lastInsertId() . "<br>";
+echo "L'id du dernier enregistrement est : " . $bdd->lastInsertId() . "<br>"; */
 // lastInsertId() est une méthode issue de la classe PDO qui permet de récupérer l'id du dernier enregistrement inséré en BDD
 
 // Faire une requête pour mettre à 2350€ le salaire de l'employé dont l'id est 876
@@ -89,5 +121,18 @@ $employes = $bdd->query("SELECT * FROM employes");
 echo "Nombre d'employés : " . $employes->rowCount() . "<br>";
 // rowCount() est une méthode de la classe PDOStatement qui permet de compter le nombre de lignes retournées par la requête
 
-$allEmployes = $employes->fetch(PDO::FETCH_ASSOC);
+//$allEmployes = $employes->fetch(PDO::FETCH_ASSOC);
 
+while ($allEmployes = $employes->fetch(PDO::FETCH_ASSOC)) {
+
+    echo "<div>";
+    echo "<p> Bonjour je suis " . $allEmployes['prenom'] . " " . $allEmployes['nom'] . " du service " . $allEmployes['service'] . " j'ai un salaire de " . $allEmployes['salaire'] . "€ et j'ai été embauché le " . $allEmployes['date_embauche'] . "</p>";
+    echo "</div><hr>";
+
+}
+
+// En utilisant fetc(), si votre requête renvoie plusieurs lignes, il faut faire une boucle pour parcourir toutes les lignes du jeu de résultats, sinon vous n'aurez que la première ligne.Par contre, si votre requête ne renvoie qu'une seule ligne, vous n'êtes pas obligé de faire une boucle.
+
+// En utilisant fetchAll(), il n'est pas nécessaire de faire une boucle car on récupère toutes les informations dans un tableau multidimensionnel. Donc on peut parcourir le tableau avec une boucle foreach ou for.
+
+// TODO : Faire une requête pour récupérer les informations de tous les employés et les afficher dans un tableau HTML
