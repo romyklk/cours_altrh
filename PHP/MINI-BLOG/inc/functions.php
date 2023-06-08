edf9a0bb417fd6616fbad956806f199efff01b77
Romuald KOULEKO <romyklk1610@gmail.com>
2023-06-08 16:52:08 +0200

    PHP

--- PHP/MINI-BLOG/inc/functions.php
+++ PHP/MINI-BLOG/inc/functions.php
@@ -1,2 +1,10 @@
 <?php 
 
+// fonction qui vérifie si l'utilisateur est connecté. Pour cela je regarde si j'ai une session user et si elle n'est pas vide
+function isLogged(){
+    if(isset($_SESSION['user']) && !empty($_SESSION['user'])){
+        return true;
+    }else{
+        return false;
+    }
+}
