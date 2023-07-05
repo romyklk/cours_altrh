04315a9071346d1189bffa7bbbe5991c5e0be5d8
Romuald KOULEKO <romyklk1610@gmail.com>
2023-07-05 16:44:04 +0200

    PHP_OO

---
+++ PHP_OO/6-Surcharge-Abstraction-Finalisation-Interface-Trait/abstraction.php
@@ -0,0 +1,12 @@
+<?php 
+/* 
+Une classe abstraite est une classe que l'on ne peut pas instancier (on ne peut pas faire d'objet à partir d'une classe abstraite). Elle est donc destinée à être héritée. Elle contient au moins une méthode abstraite. 
+
+Une méthode abstraite est une méthode qui n'a pas de contenu. Elle est destinée à être redéfinie dans les classes enfants.
+
+Pour définir une méthode abstraite, il faut que la classe soit abstraite, et il faut mettre le mot clé "abstract" devant la méthode.
+
+Une classe abstraite peut contenir des méthodes normales (non abstraites).
+
+Pour déclarer une classe abstraite, il faut mettre le mot clé "abstract" devant le mot clé "class".
+*/
