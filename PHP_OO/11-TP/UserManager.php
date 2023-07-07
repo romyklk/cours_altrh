e43565b7c0839b9dd9ebf0b80da75cf04bb81397
Romuald KOULEKO <romyklk1610@gmail.com>
2023-07-07 15:53:54 +0200

    PHP_OO

--- PHP_OO/11-TP/UserManager.php
+++ PHP_OO/11-TP/UserManager.php
@@ -30,10 +30,51 @@ class UserManager
         $query = $this->database->prepare($sql);
         $query->execute();
 
+        //setFetchMode() permet de définir le mode de récupération des données
+        //PDO::FETCH_CLASS permet de récupérer les données sous forme d'objet
+        //PDO::FETCH_PROPS_LATE permet de définir que les propriétés de l'objet seront hydratées après le constructeur
         $query->setFetchMode(PDO::FETCH_CLASS | PDO::FETCH_PROPS_LATE, 'User');
 
         $usersArray = $query->fetchAll();
 
         return $usersArray;
+    }
+
+    public function getUserById($id)
+    {
+        $sql = "SELECT * FROM membre WHERE id = :id";
+
+        $query = $this->database->prepare($sql);
+        $query->bindValue(':id', $id, PDO::PARAM_INT);
+        $query->execute();
+
+        $query->setFetchMode(PDO::FETCH_CLASS | PDO::FETCH_PROPS_LATE, 'User');
+
+        $userById = $query->fetch();
+
+        return $userById;
+    }
+
+
+    public function updateUser(User $user)
+    {
+        $sql = "UPDATE membre SET nom = :nom, prenom = :prenom, email = :email, tel = :tel WHERE id = :id";
+
+        $query = $this->database->prepare($sql);
+        $query->bindValue(':nom', $user->getNom(), PDO::PARAM_STR);
+        $query->bindValue(':prenom', $user->getPrenom(), PDO::PARAM_STR);
+        $query->bindValue(':email', $user->getEmail(), PDO::PARAM_STR);
+        $query->bindValue(':tel', $user->getTel(), PDO::PARAM_STR);
+        $query->bindValue(':id', $user->getId(), PDO::PARAM_INT);
+        $query->execute();
+    }
+
+    public function deleteUser($id)
+    {
+        $sql = "DELETE FROM membre WHERE id = :id";
+
+        $query = $this->database->prepare($sql);
+        $query->bindValue(':id', $id, PDO::PARAM_INT);
+        $query->execute();
     }
 }
